e83a2454895d8da11cfed59037b48fb28a389783
sumyyh <156509088+sumyyh@users.noreply.github.com>
2024-01-13 10:18:16 +0000

    calculator.sumaya

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def calc():
+    return render_template('index.html')
+
+@app.route("/result", methods=['GET', 'POST'])
+def calcu():
+    output = None
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        operation = request.form['operation']
+        output = cal(num1, num2, operation)
+    return render_template('result.html', operation=operation, output=output)
+
+def cal(num1, num2, operation):
+    if operation == 'addition':
+        return num1 + num2
+    elif operation == 'subtraction':
+        return num1 - num2
+    elif operation == 'multiplication':
+        return num1 * num2
+    elif operation == 'division':
+        return num1 / num2
+
+if __name__ == '__main__':
+    app.run()
